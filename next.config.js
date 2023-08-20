dff4164e216d50427998a0f08568f1c1bdd63c9c
Joao Silva <joao.devweb@gmail.com>
2023-08-20 13:11:59 -0300

    fix(next config): add new changes

--- next.config.js
+++ next.config.js
@@ -2,24 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: process.env.NODE_ENV === 'development' ? 'http' : 'https',
-        hostname:
-          process.env.NODE_ENV === 'development'
-            ? 'localhost'
-            : '**.pataspeludas.com.br',
-        port: process.env.NODE_ENV === 'development' ? '3333' : '80',
-        pathname: '/uploads/**',
-      },
-    ],
+    domains: ['localhost', 'pataspeludas.com.br', 'server.pataspeludas.com.br'],
   },
-  // images: {
-  //   domains:
-  //     process.env.NODE_ENV === 'development'
-  //       ? ['localhost']
-  //       : ['pataspeludas.com.br', 'server.pataspeludas.com.br'],
-  // },
 };
 
 module.exports = nextConfig;
